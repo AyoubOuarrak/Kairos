0a7caa60f464cf4347e49645aa953a8057f69c88
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-14 10:22:46 +0000

    Added empty serializable class

---
+++ include/serialization/serializable.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_SERIALIZATION_SERIALIZABLE_H
+#define INCLUDE_SERIALIZATION_SERIALIZABLE_H
+
+namespace kairos {
+namespace serialization {
+
+class Serializable {
+
+};
+
+
+}
+}
+#endif
