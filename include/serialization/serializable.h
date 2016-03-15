129845b0c02deefd5c9f56ab9b11ac1e8910ee4e
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-15 02:20:46 +0000

    Added serializable class

--- include/serialization/serializable.h
+++ include/serialization/serializable.h
@@ -1,11 +1,40 @@
 #ifndef INCLUDE_SERIALIZATION_SERIALIZABLE_H
 #define INCLUDE_SERIALIZATION_SERIALIZABLE_H
 
+#include "../archive/type/vector.h"
+#include "../archive/archive.h"
+
+using namespace kairos::archive;
+
 namespace kairos {
 namespace serialization {
 
+/**
+*   Abstract class Serializable
+*/
 class Serializable {
+private:
 
+protected:
+    /** default constructor */
+    Serializable() { };
+    /** disable copy constructor */
+    Serializable(const Serializable &) = delete;
+    /** disable assignment operator */
+    Serializable& operator=(const Serializable&) = delete;
+
+    /** default distructor */
+    virtual ~Serializable() { };
+
+    /** virtual pure method to serialize native types */
+    template <typename T>
+    void serialize(Archive<T>&); 
+
+    /** virtual pure method to serialize vector types */
+    template <typename T>
+    void serialize(VectorArchive<T>&);
+
+    // .... other serialize function for the other types 
 };
 
 
