fafa3c176ff6c16de7e57b14f0169c52c92d9c3d
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-16 03:30:55 +0000

    Removed templates from basic archive and archive

--- include/archive/templates/archive.templates.h
+++ include/archive/templates/archive.templates.h
@@ -1,64 +1,30 @@
-
-/**
-*   Default constructor 
-*/
-template <class item_type>
-Archive<item_type>::Archive() : input_archive(), output_archive(), archivePath(".") {
- 
-}
-
-/**
-*   copy operator
-*/
-template <class item_type>
-Archive<item_type>::Archive(const Archive& archive_) {
-
-} 
-
-/**
-*   Second constructor 
-*/
-template <class item_type>
-Archive<item_type>::Archive(std::string path) : input_archive(), output_archive() {
-    archivePath = path;
-}
-
-/**
-*   Default distructor 
-*/
-template <class item_type>
-Archive<item_type>::~Archive() {
-
-}
-
 /**
 *   Overloading operator <<
-*/
-template <class item_type> 
-Archive<item_type>& Archive<item_type>::operator<<(const item_type& item) {
+*/ 
+template <class item_type>
+void Archive::add(const item_type& item) {
     if(io_archive::isInOpen())
         io_archive::closeInStream();
 
     if(!io_archive::isOutOpen())
         io_archive::openOutStream(std::ios::out | std::ios::app);
 
-    //OArchive<item_type>::stream << item;
-    //OArchive<item_type>::stream << " ";
+    OArchive::stream << item;
+    OArchive::stream << " ";
     return *this;
 }
 
 /**
 *   Overloading operator >> 
-*/
-template <class item_type> 
-Archive<item_type>& Archive<item_type>::operator>>(item_type& item) { 
+*/ 
+template <class item_type>
+item_type Archive::get() { 
     if(io_archive::isOutOpen())
         io_archive::closeOutStream();
 
     if(!io_archive::isInOpen())
         io_archive::openInStream(std::ios::in);
 
-    //IArchive<item_type>::stream >> item;
+    IArchive::stream >> item;
     return *this;
 }
-
