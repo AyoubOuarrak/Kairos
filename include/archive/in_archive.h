c5fd016013815819eeb87777514216cf7c84e334
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-18 20:55:29 +0000

    Splitted In/Out archive definition and added virtual pure getter and putter

---
+++ include/archive/in_archive.h
@@ -0,0 +1,46 @@
+#ifndef ARCHIVE_INPUT_ARCHIVE_H
+#define ARCHIVE_INPUT_ARCHIVE_H
+
+#include <string>
+#include <fstream>
+
+namespace kairos {
+namespace archive {
+
+/**
+*   Input Archive class, extend Basic archive
+*/
+class InArchive : protected virtual BasicArchive {
+public:
+    /** default constructor */
+    InArchive();
+
+    /** copy operator */
+    InArchive(const IArchive&);
+    
+    /** second constructor */
+    explicit InArchive(std::ifstream steam);
+    
+    /** default distructor */
+    virtual ~InArchive() = default;
+
+    /** pure overloading get operator for scalar types */
+    virtual void get(int& dest) = 0;
+    virtual void get(long& dest) = 0;
+    virtual void get(long long& dest) = 0;
+    virtual void get(double& dest) = 0;
+    virtual void get(char& dest) = 0;
+    virtual void get(long long& dest) = 0;
+};
+
+/**
+*   >> overloding operator
+*/
+template <class T>
+InArchive& operator>>(InArchive& in, T item) {
+    in.get(item);
+}
+
+}
+}
+#endif
---
+++ include/archive/out_archive.h
@@ -0,0 +1,47 @@
+#ifndef ARCHIVE_OUTPUT_ARCHIVE_H
+#define ARCHIVE_OUTPUT_ARCHIVE_H
+
+#include <string>
+#include <fstream>
+
+namespace kairos {
+namespace archive {
+
+/**
+*   Ouput Archive class, extend Basic archive 
+*/
+class OutArchive : protected virtual BasicArchive {
+public:
+    /** default constructor */
+    OutArchive();
+
+    /** copy operator */
+    OutArchive(const OArchive&);
+
+    /** second constructor */
+    explicit OutArchive(std::ofstream stream);
+    
+    /** default distructor */
+    virtual ~OutArchive();
+
+    /** pure overloading put operator for scalar types */
+    virtual void put(int src) = 0;
+    virtual void put(long src) = 0;
+    virtual void put(long long src) = 0;
+    virtual void put(double src) = 0;
+    virtual void put(char src) = 0;
+    virtual void put(long long src) = 0;
+};
+
+/**
+*   << overloding operator
+*/
+template <class T>
+OutArchive& operator<<(OutArchive& out, T item) {
+    out.put(item);
+}
+
+}
+}
+
+#endif
