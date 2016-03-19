a7d1ef761d93267d1c745e70a586e5ae5ab2c18f
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-19 20:51:41 +0000

    Added abstract exception class

---
+++ include/exception/exception.h
@@ -0,0 +1,37 @@
+#ifndef INCLUDE_EXCEPTION_EXCEPTION_H
+#define INCLUDE_EXCEPTION_EXCEPTION_H
+
+#include <exception>
+#include <string>
+
+namespace kairos {
+namespace exception {
+
+/**
+*   Abstract class for exceptions
+*/
+class Exception : public std::exception {
+private:
+    /** exception message */
+    std::string exp;
+
+public:
+    /** default constructor without message is not allowed */
+    Exception() = delete;
+
+    /** constructor with char* message */
+    explicit Exception(char* message);
+
+    /** constructor with std string message */
+    explicit Exception(std::string message);
+
+    /** default distructor */
+    virtual ~Exception();
+
+    /** return the exception message */
+    virtual const char* what() const throw() = 0;
+};
+
+}
+}
+#endif
