521715869a7113c6bd3234b815d5dfb6d4e9e3d4
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-22 19:09:08 +0000

    Moved non templatic method of built_in to cpp file

--- include/serialization/type/templates/built_in.templates.h
+++ include/serialization/type/templates/built_in.templates.h
@@ -1,36 +1,5 @@
 
 /**
-*   Default constructor
-*/
-BuiltIn::BuiltIn() {
-    archive = new TextArchive();
-}
-
-BuiltIn::BuiltIn(Archive* _archive) {
-    archive = _archive;
-}
-/**
-*   Copy constructor
-*/
-BuiltIn::BuiltIn(const BuiltIn& built_in) {
-
-}
-
-/**
-*   Default dtor
-*/
-BuiltIn::~BuiltIn() {
-
-}
-
-/**
-*   Assignment operator
-*/
-Archive BuiltIn::operator=(const BuiltIn&) {
-
-}
-
-/**
 *   Save a built-in type in the vector
 */
 template <class T>
