aa8aa4b1946e706079c5e109f20d69fa4a93c4ee
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-26 03:59:26 +0000

    Changes in cmake file

--- kairos.h
+++ kairos.h
@@ -4,6 +4,6 @@
 #include "include/serialization/serializable.h"
 #include "include/serialization/serialization.h"
 #include "include/serialization/type/built_in.h"
-
+#include "include/serialization/type/vector.h"
 
 #endif //KAIROS_KAIROS_H
