d4efca5dbba015681de1aba055cc4dd51dbe4308
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-26 01:47:29 +0000

    Added the kairos header

---
+++ kairos.h
@@ -0,0 +1,9 @@
+#ifndef KAIROS_KAIROS_H
+#define KAIROS_KAIROS_H
+
+#include "include/serialization/serializable.h"
+#include "include/serialization/serialization.h"
+#include "include/serialization/type/built_in.h"
+
+
+#endif //KAIROS_KAIROS_H
