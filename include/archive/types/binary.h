f28b343597e0e6054c736dc9925d881c49ab4430
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-26 01:26:42 +0000

    Moved method for text archive in private space

--- include/archive/types/binary.h
+++ include/archive/types/binary.h
@@ -10,6 +10,21 @@ namespace archive {
 
 class BinaryArchive : public Archive {
 private:
+    /** empty body for text serialization */
+    virtual void put(int src);
+    virtual void put(long src);
+    virtual void put(long long src);
+    virtual void put(double src);
+    virtual void put(char src);
+    virtual void put(bool src);
+
+    virtual void get(int& dest);
+    virtual void get(long& dest);
+    virtual void get(long long& dest);
+    virtual void get(double& dest);
+    virtual void get(char& dest);
+    virtual void get(bool& dest);
+
 public:
     /** default ctor */
     BinaryArchive();
@@ -23,6 +38,7 @@ public:
     /** override for binary serialization of sz bytes */
     virtual void get(char* p, std::size_t sz);
     virtual void put(char* p, std::size_t sz);
+
 };
 
 }
