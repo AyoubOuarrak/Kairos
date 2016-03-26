28fa4865705b59b7ad10b89cbc2f3ad033ed2ae5
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-26 01:29:22 +0000

    Implemented empty body method for text archive

--- src/archive/types/binary.cpp
+++ src/archive/types/binary.cpp
@@ -1,5 +1,5 @@
 #include "../../include/archive/types/binary.h"
-
+#include <iostream>
 namespace kairos {
 namespace archive {
 
@@ -16,7 +16,7 @@ BinaryArchive::BinaryArchive() : Archive() {
 *   Copy operator of text archive
 */
 BinaryArchive::BinaryArchive(const BinaryArchive&) {
-    
+
 }
 
 /**
@@ -40,5 +40,91 @@ void BinaryArchive::put(char* p, std::size_t sz) {
     outStream.write(p, sz);
 }
 
+
+/**
+*   Put int in the text archive
+*/
+void BinaryArchive::put(int) {
+    // empty body
+}
+
+/**
+*   Put long in the text archive
+*/
+void BinaryArchive::put(long) {
+    //empty body
+}
+
+/**
+*   Put long long in the text archive
+*/
+void BinaryArchive::put(long long) {
+    // empty body
+}
+
+/**
+*   Put double in the text archive
+*/
+void BinaryArchive::put(double) {
+    // empty body
+}
+
+/**
+*   Put char in the text archive
+*/
+void BinaryArchive::put(char) {
+    // empty body
+}
+
+/**
+*   Put bool in the text archive
+*/
+void BinaryArchive::put(bool) {
+    // empty body
+}
+
+
+/**
+*   Get int from the archive
+*/
+void BinaryArchive::get(int&) {
+    // empty body
+}
+
+/**
+*   Get long from the archive
+*/
+void BinaryArchive::get(long&) {
+    // empty body
+}
+
+/**
+*   Get long long from the archive
+*/
+void BinaryArchive::get(long long&) {
+    // empty body
+}
+
+/**
+*   Get double from the archive
+
+*/
+void BinaryArchive::get(double&) {
+    // empty body
+}
+
+/**
+*   Get bool from the archive
+*/
+void BinaryArchive::get(bool&) {
+    // empty body
+}
+
+/**
+*   Get bool from the archive
+*/
+void BinaryArchive::get(char&) {
+    // mepty body
+}
 }
 }
