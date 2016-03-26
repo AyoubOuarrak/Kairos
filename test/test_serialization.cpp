1e8a7b2e6a503cfff58ea69a51e28859188c84b4
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-26 03:58:06 +0000

    Added test for vector serialization

--- test/test_serialization.cpp
+++ test/test_serialization.cpp
@@ -3,7 +3,9 @@
 using namespace kairos;
 using namespace serialization;
 
-
+/**
+ *  1) TEST BINARY SERIALIZATION USING BUILTIN ARCHIVE
+ */
 class User : public Serializable, public Serialization {
 private:
     int id;
@@ -31,6 +33,9 @@ public:
     }
 };
 
+/**
+ *  2) TEST TEXT SERIALIZATION USING BUILTIN ARCHIVE
+ */
 class Player : public Serializable, public Serialization {
 private:
     float score;
@@ -57,14 +62,40 @@ public:
     }
 };
 
+/**
+ *  3) TEST VECTOR SERIALIZATION
+ */
+class Fizz : public Serializable, public Serialization {
+private:
+    std::vector<int> v;
+
+public:
+    Fizz() {
+        v.push_back(2);
+        v.push_back(9);
+        v.push_back(123);
+        v.push_back(29342);
+        v.push_back(12);
+    }
+
+    void serialize() {
+        /** by default use a text archive */
+        Vector vectorSer;
+
+        vectorSer << v;
+    }
+};
+
 int main() {
     User* user = new User(0, 23);
     Player* player = new Player(3.43);
     Player player0;
+    Fizz fizz;
 
     Serialization::createCheckpoint(user);
     Serialization::createCheckpoint(player);
     Serialization::createCheckpoint(&player0);
+    Serialization::createCheckpoint(&fizz);
 
     
 }
