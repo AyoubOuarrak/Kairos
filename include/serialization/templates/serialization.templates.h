1f16282b35ae39f6c360157c135bfc72cf19040a
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-03-27 23:12:57 +0100

    Restore serialization method is now templatic

---
+++ include/serialization/templates/serialization.templates.h
@@ -0,0 +1,9 @@
+
+/**
+*   Restore objects
+*/
+template <class T>
+void Serialization::restore() {
+    //std::string file = serializedObjectsFiles.at(serializableObjects.at(*T));
+    //std::cout << file << std::endl;
+}
