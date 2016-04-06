9993b4752e4e0a1ddc211aeecd710f2ea3c20333
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-04-06 02:10:34 +0100

    A lot of bug fix

--- include/archive/types/text.h
+++ include/archive/types/text.h
@@ -11,37 +11,42 @@ namespace archive {
 class TextArchive : public Archive {
 private:
     /** empty body for binary serialization method */
-    virtual void get(char*, std::size_t);
-    virtual void put(char*, std::size_t);
+    void get(char*, std::size_t);
+    void put(char*, std::size_t);
 
 public:
     /** default ctor */
     TextArchive();
 
+    /** constructor that take the name of an archive */
+    explicit TextArchive(const std::string);
+
     /** copy constructor */
     TextArchive(const TextArchive&);
 
     /** assignment operator */
-    Archive operator=(const TextArchive&);
+    IOArchive operator=(const TextArchive&);
     
     /** default dtor */
     virtual ~TextArchive();
 
     /** override for put functions */
-    virtual void put(int src);
-    virtual void put(long src);
-    virtual void put(long long src);
-    virtual void put(double src);
-    virtual void put(char src);
-    virtual void put(bool src);
+    void put(int src);
+    void put(long src);
+    void put(long long src);
+    void put(double src);
+    void put(float dest);
+    void put(char src);
+    void put(bool src);
 
     /** override for get functions */
-    virtual void get(int& dest);
-    virtual void get(long& dest);
-    virtual void get(long long& dest);
-    virtual void get(double& dest);
-    virtual void get(char& dest);
-    virtual void get(bool& dest);
+    void get(int& dest);
+    void get(long& dest);
+    void get(long long& dest);
+    void get(double& dest);
+    void get(float& dest);
+    void get(char& dest);
+    void get(bool& dest);
 
 };
 
