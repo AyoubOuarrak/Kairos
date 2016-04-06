75cbd1637702f70b0589c11afd992fb6754f8743
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-04-06 02:14:28 +0100

    A lot of bug fix

--- src/archive/types/binary.cpp
+++ src/archive/types/binary.cpp
@@ -1,4 +1,5 @@
 #include "../../include/archive/types/binary.h"
+#include "../../../include/floating_point.h"
 #include <iostream>
 namespace kairos {
 namespace archive {
@@ -11,6 +12,13 @@ BinaryArchive::BinaryArchive() : Archive() {
     openOutStream(std::ios::out | std::ios::binary);
     std::rename("archive", "archive.binary");
     Archive::rename("archive.binary");
+}
+
+/**
+*   Constructor that open an input archive with the given fileName
+*/
+BinaryArchive::BinaryArchive(const std::string fileName) : Archive(fileName) {
+
 }
 
 /**
@@ -43,89 +51,110 @@ void BinaryArchive::put(char* p, std::size_t sz) {
 
 
 /**
-*   Put int in the text archive
+*   Put int in the Binary archive
 */
-void BinaryArchive::put(int) {
-    // empty body
+void BinaryArchive::put(int src) {
+    put((char*)&src, sizeof(src));
 }
 
 /**
-*   Put long in the text archive
+*   Put long in the Binary archive
 */
-void BinaryArchive::put(long) {
-    //empty body
+void BinaryArchive::put(long src) {
+    put((char*)&src, sizeof(src));
 }
 
 /**
-*   Put long long in the text archive
+*   Put long long in the Binary archive
 */
-void BinaryArchive::put(long long) {
-    // empty body
+void BinaryArchive::put(long long src) {
+    put((char*)&src, sizeof(src));
 }
 
 /**
-*   Put double in the text archive
+*   Put double in the Binary archive
 */
-void BinaryArchive::put(double) {
-    // empty body
+void BinaryArchive::put(double src) {
+    uint64_t buffer = to_uint64(src);
+    put((char*)&buffer, sizeof(src));
 }
 
 /**
-*   Put char in the text archive
+*   Put float from archive
 */
-void BinaryArchive::put(char) {
-    // empty body
+void BinaryArchive::put(float src) {
+    uint32_t buffer =  to_uint32(src);
+    put((char*)&buffer, sizeof(src));
 }
 
 /**
-*   Put bool in the text archive
+*   Put char in the Binary archive
 */
-void BinaryArchive::put(bool) {
-    // empty body
+void BinaryArchive::put(char src) {
+    put((char*)&src, sizeof(src));
+}
+
+/**
+*   Put bool in the Binary archive
+*/
+void BinaryArchive::put(bool src) {
+    put((char*)&src, sizeof(src));
 }
 
 
 /**
 *   Get int from the archive
 */
-void BinaryArchive::get(int&) {
-    // empty body
+void BinaryArchive::get(int& dst) {
+    get((char*)dst, sizeof(dst));
 }
 
 /**
 *   Get long from the archive
 */
-void BinaryArchive::get(long&) {
-    // empty body
+void BinaryArchive::get(long& dst) {
+    get((char*)dst, sizeof(dst));
 }
 
 /**
 *   Get long long from the archive
 */
-void BinaryArchive::get(long long&) {
-    // empty body
+void BinaryArchive::get(long long& dst) {
+    get((char*)dst, sizeof(dst));
 }
 
 /**
 *   Get double from the archive
 
 */
-void BinaryArchive::get(double&) {
-    // empty body
+void BinaryArchive::get(double& dst) {
+    char* buffer;
+    get(buffer, sizeof(dst));
+    dst = from_uint64((uint64_t)buffer);
+}
+
+/**
+*   Get float from archive
+*/
+void BinaryArchive::get(float& dst) {
+    char* buffer;
+    get(buffer, sizeof(dst));
+    dst = from_uint32((uint64_t)buffer);
 }
 
 /**
 *   Get bool from the archive
 */
-void BinaryArchive::get(bool&) {
-    // empty body
+void BinaryArchive::get(bool& dst) {
+    get((char*)dst, sizeof(dst));
 }
 
 /**
 *   Get bool from the archive
 */
-void BinaryArchive::get(char&) {
-    // mepty body
+void BinaryArchive::get(char& dst) {
+    get((char*)dst, sizeof(dst));
 }
+
 }
 }
