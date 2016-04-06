0c616171f13a7bd3f778422d4a88074beaaf6b9c
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-04-06 02:13:21 +0100

    A lot of bug fix

--- include/serialization/serialization.h
+++ include/serialization/serialization.h
@@ -15,33 +15,46 @@ using namespace exception;
 */
 class Serialization : public SerializationException {
 private:
+    /** serialization configuration file */
+    std::ifstream serializationConf;
+
+    /** the Serializer has been initialize?*/
+    static bool initialized;
+
     /** list of serializable objects */
     static std::map<Serializable*, uint16_t> serializableObjects;
+    static std::map<Serializable*, std::string> ObjectjFormatSerialization;
     static std::map<uint16_t, std::string> serializedObjectsFiles;
 
+    /** method to init serialization */
+    void initSerialization();
+
 protected:
+    /** default constructor */
+    Serialization();
+
     /** add obj to the list of object that need to be serialized */
     void registerType(Serializable*);
+    void registerType(Serializable*, std::string);
 
     /** get the serializable object from the id */
     Serializable* getObject(uint16_t id);
+
+    /** type of serialization */
+    const static std::string BINARY;
+    const static std::string TEXT;
 
 public:
     /** call serialize method for a specific object */
     static void createCheckpoint(Serializable*);
 
     /** restore object */
-    template <class T>
-    static void restore();
-
-    /** load the status of a object */
-    Serializable* loadCheckpoint();
+    static void restore(Serializable*);
 
     /** serialize all object in the list */
     void checkpoint();
 };
 
-#include "templates/serialization.templates.h"
 }
 }
 
