4470426e2e87c9e37ec2fbb23ce01c199598147b
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-04-09 15:46:43 +0100

    Changes in kairos header

--- kairos.h
+++ kairos.h
@@ -1,8 +1,6 @@
 #ifndef KAIROS_KAIROS_H
 #define KAIROS_KAIROS_H
 
-#include "include/serialization/serializable.h"
 #include "include/serialization/serialization.h"
-#include "include/serialization/type/vector.h"
 
 #endif //KAIROS_KAIROS_H
