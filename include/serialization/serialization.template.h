8d6601508896def7a40e25784007535645b6f019
AyoubOuarrak <andreyvondorcon@gmail.com>
2016-04-09 15:42:13 +0100

    Moved templatic serialization restore

---
+++ include/serialization/serialization.template.h
@@ -0,0 +1,49 @@
+
+/**
+*   Restore a serializable object
+*/
+template <class ClassType>
+std::list<ClassType*> Serialization::restore() {
+    serializationIndexOut.close();
+    serializationIndexIn.close();
+    serializationIndexIn.open("serialization.index", std::ios::in);
+
+    std::string className;
+    std::string classSerializationFormat;
+    std::string classSerializationFile;
+    std::list<ClassType*> serializedObjects;
+
+    /** iterate over the serialization index */
+    while(serializationIndexIn >> className >> classSerializationFormat >> classSerializationFile) {
+        /** if the classname is the same the template type */
+        if(utils::is_same<ClassType>(className)) {
+            std::cout << className << std::endl;
+            /** create a new empty object, get the file name of the archive and start to load data */
+            ClassType* object = new ClassType;
+
+            try {
+                if (classSerializationFormat == Serialization::TEXT) {
+                    TextArchive archive(classSerializationFile);
+                    object->deserialize(archive);
+                }
+
+                if (classSerializationFormat == Serialization::BINARY) {
+                    BinaryArchive archive(classSerializationFile);
+                    object->deserialize(archive);
+                }
+
+                serializedObjects.push_back(object);
+                registerType(object, classSerializationFormat);
+
+            } catch (std::out_of_range exp) {
+                std::cout << exp.what() << std::endl;
+                throw new SerializationException("errors trying to deserialize " + className);
+
+            } catch (SerializationException* exp) {
+                throw exp;
+            }
+        }
+    }
+
+    return serializedObjects;
+}
