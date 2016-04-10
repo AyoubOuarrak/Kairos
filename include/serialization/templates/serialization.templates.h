9655c75683d358dfefd795f606c0a9f35c3cc66f
Ayoub Ouarrak <andreyvondorcon@gmail.com>
2016-04-10 17:38:53 +0100

    Delete serialization.templates.h

--- include/serialization/templates/serialization.templates.h
+++
@@ -1,9 +0,0 @@
-
-/**
-*   Restore objects
-*/
-template <class T>
-void Serialization::restore() {
-    //std::string file = serializedObjectsFiles.at(serializableObjects.at(*T));
-    //std::cout << file << std::endl;
-}
